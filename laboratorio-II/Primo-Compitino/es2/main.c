35a5211358e3c4a8d053722d8bcb1274e0075c1a
Mitra98t <personalmailfm98@gmail.com>
2021-10-12 15:21:39 +0200

    Cifrario di cesare es2 primo compito

--- laboratorio-II/Primo-Compitino/es2/main.c
+++ laboratorio-II/Primo-Compitino/es2/main.c
@@ -1,21 +1,54 @@
 #include <stdio.h>
 #include <string.h>
-#include <ctype.h>
+#include <stdlib.h>
 
 #define DIM 15
+#define SHIFT 4
+
+char *encode(char *);
+char *decode(char *);
 
 int main(void)
 {
-    char *s[DIM];
-    scanf("%s", &s);
-
-    for (size_t i = 0; i < strlen(*s); i++)
+    char s[DIM];
+    scanf("%s", s);
+    printf("%s è lunga: %ld", s, strlen(s));
+    for (size_t i = 0; i < strlen(s); i++)
     {
-        printf("ASCII value of %c = %d", s[i], s[i]);
+        // printf("ASCII value of %c = %d", s[i], s[i]);
+        if (s[i] >= 97)
+            s[i] = s[i] - 32;
     }
 
-    // printf("\n%s\n", res);
+    printf("\nEncode %s\n", encode(s));
+    printf("\nDecode %s\n", decode(s));
 
-
+    printf("\n%s\n", s);
     return 0;
+}
+
+char *encode(char *s)
+{
+    char *str = (char *)malloc((strlen(s) + 1) * sizeof(char));
+    strcpy(str, s);
+
+    for (size_t i = 0; i < strlen(str); i++)
+    {
+
+        str[i] = (((str[i] - 65) + (SHIFT )) % 26) + 65;
+    }
+    return str;
+}
+
+char *decode(char *s)
+{
+    char *str = (char *)malloc((strlen(s) + 1) * sizeof(char));
+    strcpy(str, s);
+
+    for (size_t i = 0; i < strlen(str); i++)
+    {
+
+        str[i] = (((str[i] - (SHIFT )) - 13) % 26) + 65;
+    }
+    return str;
 }
