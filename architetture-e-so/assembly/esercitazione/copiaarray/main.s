fc6a6b304d5e270d0bf271cbd8ca087978f574a3
Mitra98t <personalmailfm98@gmail.com>
2021-11-17 15:18:26 +0100

    copy array assembly

--- architetture-e-so/assembly/esercitazione/copiaarray/main.s
+++ architetture-e-so/assembly/esercitazione/copiaarray/main.s
@@ -1,6 +1,7 @@
 .data
-a1: .word 1,2,3,4   @ dichiaro array statico
-a2: .space 16       @ alloco 16 byte
+a1:     .word 1,2,3,4   @ dichiaro array statico
+a2:     .space 16       @ alloco 16 byte
+str:    .string "el: %d\n"
 
 .text
 .global main
@@ -10,4 +11,12 @@ main:
     ldr r1, =a2
     push {lr}
     bl copyarray
+    ldr r0, =str
+    ldr r1, =a2
+    mov r3, #0
+loop:
+    bl printf
+    add r3, r3, #1
+    cmp r3, #3
+    bne loop
     pop {pc}    
