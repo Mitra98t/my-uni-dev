5d6a7dba211b2e97a5e123530e7469ae74d090fb
Mitra98t <personalmailfm98@gmail.com>
2021-11-17 10:17:52 +0100

    sync

--- architetture-e-so/assembly/esercitazione/palindromo/main.s
+++ architetture-e-so/assembly/esercitazione/palindromo/main.s
@@ -1,13 +1,14 @@
 .data
-s:  .string "ciao"
-
+s:      .string "ciao"
+res:    .string "caratteri: %d\n"
 .text
 .global main
 
 main:
     push {lr}
+    ldr r0, =s
     bl pal
     mov r1, r0 
-    ldr r0, =s
+    ldr r0, =res
     bl printf
     pop {pc}
