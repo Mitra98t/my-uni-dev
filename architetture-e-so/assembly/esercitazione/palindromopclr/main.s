66a90ce90b91f54b911bedfb23b8ebd18281a6b5
Mitra98t <personalmailfm98@gmail.com>
2021-11-17 11:48:14 +0100

    assembly stuff

---
+++ architetture-e-so/assembly/esercitazione/palindromopclr/main.s
@@ -0,0 +1,15 @@
+.data
+s:      .string "cazzo"
+res:    .string "palindromo: %d\n"
+
+.text
+.global main
+
+main:
+    push {lr}
+    ldr r0, =s
+    bl pal
+    mov r1, r0 
+    ldr r0, =res
+    bl printf
+    pop {pc}
---
+++ architetture-e-so/assembly/esercitazione/palindromopclr/palindromo.s
@@ -0,0 +1,36 @@
+@ r0 => string index
+
+.text
+.global pal
+
+pal:
+    push {r0}
+    b getlastpointer
+    mov r1, r0
+    pop {r0}
+    cmp r1, r0
+    movle r0, #1
+    movle pc, lr
+    b confronta
+    mov pc, lr
+
+getlastpointer:
+    ldrb r2, [r0], #1
+    cmp r2, #0
+    bne getlastpointer
+    sub r0, r0, #2
+    mov pc, lr
+
+confronta:
+    ldrb r2, [r0]
+    ldrb r3, [r1]
+    cmp r2, r3
+    movne r0, #0
+    movne pc, lr
+    sub r2, r1, r0
+    cmp r2, #1
+    movle r0, #1
+    movle pc, lr
+    add r0, r0, #1
+    sub r1, r1, #1
+    b confronta
