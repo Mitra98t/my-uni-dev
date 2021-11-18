4d2e8b714efa9d505f1bd0ac1d4874f02486962a
Mitra98t <personalmailfm98@gmail.com>
2021-11-18 15:04:02 +0100

    sync

--- architetture-e-so/assembly/esercitazione/sommapari/sommapari.s
+++ architetture-e-so/assembly/esercitazione/sommapari/sommapari.s
@@ -9,7 +9,10 @@ loop:
     ldr r2, [r0], #4
     and r3, r2, #1
     cmp r3, #1
-    addeq r4, r4, r2
+    addne r4, r4, r2
     sub r1, r1, #1
+    cmp r1, #0
+    bne loop
+    mov r0, r4
     pop {r4}
     mov pc, lr
