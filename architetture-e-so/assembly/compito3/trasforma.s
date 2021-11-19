c62a61e1286ba68752fd33951f105ccdb672a462
Federico Miraglia <51447075+Mitra98t@users.noreply.github.com>
2021-11-19 17:26:13 +0100

    compito 3 func

--- architetture-e-so/assembly/compito3/trasforma.s
+++ architetture-e-so/assembly/compito3/trasforma.s
@@ -26,29 +26,3 @@ fort:
 endt:
     pop {r0, r4, r5}
     mov pc, lr
-
-@ r0 => carattere
-@ r1 => from
-@ r2 => to
-@ r3 => n
-
-cambia:
-    push {r4, r5}
-    mov r5, #0
-forsearch:
-    ldrb r4, [r1, r5]
-    cmp r4, r0
-    beq sostituto
-    add r5, r5, #1
-    cmp r5, r3
-    bge endcambia
-    b forsearch
-
-sostituto:
-    cmp r5, r3
-    bge endcambia
-    ldrb r0, [r2, r5]
-
-endcambia:
-    pop {r4, r5}
-    mov pc, lr
