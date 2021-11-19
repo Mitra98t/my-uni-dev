65060088f01d3dbe1b2e5f6255aa0836f1e9cf75
Federico Miraglia <51447075+Mitra98t@users.noreply.github.com>
2021-11-19 17:36:58 +0100

    compito 3 func

--- architetture-e-so/assembly/compito3/cambia.s
+++ architetture-e-so/assembly/compito3/cambia.s
@@ -8,20 +8,20 @@
 
 cambia:
     push {r4, r5}
-    mov r5, #0
+    mov r5, #0          @ intendo usare r5 come contatore
 forsearch:
-    ldrb r4, [r1, r5]
-    cmp r4, r0
-    beq sostituto
+    ldrb r4, [r1, r5]   @ carico in r4 il carattere di dell'array r1 con offset r5
+    cmp r4, r0          @ confronto con il carattere in input
+    beq sostituto       @ se corrisponde vado alla sostituzione
     add r5, r5, #1
-    cmp r5, r3
-    bge endcambia
-    b forsearch
+    cmp r5, r3          @ controllo fine dell'array
+    bge endcambia       @ se array finito vado a endcambia
+    b forsearch         
 
 sostituto:
-    cmp r5, r3
-    bge endcambia
-    ldrb r0, [r2, r5]
+    cmp r5, r3          @ controllo se il contatore punta oltre l'array
+    bge endcambia       @ se si vado a endcambia
+    ldrb r0, [r2, r5]   @ carico in r0 il nuovo carattere
 
 endcambia:
     pop {r4, r5}
--- architetture-e-so/assembly/compito3/trasforma.s
+++ architetture-e-so/assembly/compito3/trasforma.s
@@ -7,22 +7,22 @@
 .global trasforma
 
 trasforma:
-    push {r0, r4, r5}
+    push {r0, r4, r5}   @ push r0 per mantenere il puntatore alla testa
 fort:
-    ldrb r4, [r0]
-    cmp r4, #0
-    beq endt
-    push {lr}
-    push {r0, r4}
-    mov r0, r4
-    bl cambia
-    mov r4, r0
-    pop {r0, r5}
+    ldrb r4, [r0]       @ carico primo carattere
+    cmp r4, #0          @ confronto con end of string
+    beq endt            @ se fine stringa chiudo
+    push {lr}           
+    push {r0, r4}       
+    mov r0, r4          @ metto il carattere considerato in r0
+    bl cambia           
+    mov r4, r0          @ metto il carattere restituito da cambia in r4
+    pop {r0, r5}        
     pop {lr}
-    cmp r4, r5
-    strneb r4, [r0]
-    add r0, r0, #1
+    cmp r4, r5          @ confronto il nuovo con il vecchio carattere
+    strneb r4, [r0]     @ se sono diversi sostituisco
+    add r0, r0, #1      @ incremento il puntatore
     b fort
 endt:
-    pop {r0, r4, r5}
-    mov pc, lr
+    pop {r0, r4, r5}    
+    mov pc, lr          
