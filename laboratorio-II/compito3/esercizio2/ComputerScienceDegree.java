d5f12b0f80a21303233ad2adf1c9196b0b9deb31
Mitra98t <personalmailfm98@gmail.com>
2022-01-27 11:55:18 +0100

    Better load computer sciance degree esercizio 2 compito 3

--- laboratorio-II/compito3/esercizio2/ComputerScienceDegree.java
+++ laboratorio-II/compito3/esercizio2/ComputerScienceDegree.java
@@ -17,61 +17,32 @@ public class ComputerScienceDegree {
 
     void load(String filename) throws IOException {
         try (BufferedReader bf = new BufferedReader(new FileReader(filename))) {
-            String riga = bf.readLine();
-            while (riga != null) {
-                String[] values = riga.split(",");
-                Student s = new Student(values[1], Integer.parseInt(values[0].trim()));
+            String riga = "";
+            String[] values;
+            String[] votiScr;
+            Student s;
+            AbstractExam esame;
+            while ((riga = bf.readLine()) != null) {
+                values = riga.split(",");
+                s = new Student(values[1], Integer.parseInt(values[0].trim()));
                 for (int i = 2; i < values.length; i += 4) {
-                    String[] votoScritti = values[i + 3].split(";");
-                    if (votoScritti.length > 1) {
-                        ContinuousEvaluationExam esame = new ContinuousEvaluationExam(values[i],
-                                Integer.parseInt(values[i + 1].trim()));
+                    votiScr = values[i + 3].split(";");
+                    if (votiScr.length > 1) {
+                        esame = new ContinuousEvaluationExam(values[i], Integer.parseInt(values[i + 1].trim()));
                         esame.setOralGrade(Integer.parseInt(values[i + 2].trim()));
-                        for (String vS : votoScritti) {
-                            esame.addContinuousEvaluationGrade(Integer.parseInt(vS.trim()));
+                        for (String string : votiScr) {
+                            ((ContinuousEvaluationExam) esame)
+                                    .addContinuousEvaluationGrade(Integer.parseInt(string.trim()));
                         }
-                        s.addExam(esame);
                     } else {
-                        WrittenAndOralExam esame = new WrittenAndOralExam(values[i],
-                                Integer.parseInt(values[i + 1].trim()));
+                        esame = new WrittenAndOralExam(values[i], Integer.parseInt(values[i + 1].trim()));
                         esame.setOralGrade(Integer.parseInt(values[i + 2].trim()));
-                        esame.setWrittenGrade(Integer.parseInt(values[i + 3].trim()));
-                        s.addExam(esame);
+                        ((WrittenAndOralExam) esame).setWrittenGrade(Integer.parseInt(values[i + 3].trim()));
                     }
+                    s.addExam(esame);
                 }
-                students.add(s);
-                riga = bf.readLine();
+                addStudent(s);
             }
-
-            // while (riga != null) {
-            // System.out.println(riga);
-            // String[] values = riga.split(",");
-            // Student toAdd = new Student(values[1], Integer.parseInt(values[0]));
-            // System.out.println("RIGA nome: " + values[1]);
-            // if (values.length > 2) {
-            // for (int i = 2; i < values.length; i += 4) {
-            // String[] votiScritti = values[i + 3].split(";");
-            // if (votiScritti.length > 1) {
-            // ContinuousEvaluationExam exam = new ContinuousEvaluationExam(values[i],
-            // Integer.parseInt(values[i + 1]));
-            // exam.setOralGrade(Integer.parseInt(values[i + 2]));
-            // for (String string : votiScritti) {
-            // exam.addContinuousEvaluationGrade(Integer.parseInt(string));
-            // }
-            // toAdd.addExam(exam);
-            // } else {
-            // WrittenAndOralExam exam = new WrittenAndOralExam(values[i],
-            // Integer.parseInt(values[i + 1]));
-            // exam.setOralGrade(Integer.parseInt(values[i + 2]));
-            // exam.setWrittenGrade(Integer.parseInt(values[i + 3]));
-            // toAdd.addExam(exam);
-            // }
-            // }
-            // }
-            // System.out.println(toAdd);
-            // addStudent(toAdd);
-            // riga = bf.readLine();
-            // }
         } catch (Exception e) {
             e.printStackTrace();
             throw new IOException();
--- laboratorio-II/compito3/esercizio2/main.java
+++ laboratorio-II/compito3/esercizio2/main.java
@@ -1,38 +1,17 @@
+import java.io.IOException;
+
 /**
  * main
  */
 public class main {
     public static void main(String[] args) {
-        ContinuousEvaluationExam esame = new ContinuousEvaluationExam("ciao", 6);
-
-        esame.setOralGrade(19);
-        esame.addContinuousEvaluationGrade(19);
-        esame.addContinuousEvaluationGrade(19);
-        esame.addContinuousEvaluationGrade(19);
-
-        System.out.println("Voto primo esame: " + esame.getGrade());
-
-        WrittenAndOralExam esame2 = new WrittenAndOralExam("ciao2", 6);
-
-        esame2.setOralGrade(15);
-        esame2.setWrittenGrade(15);
-
-        System.out.println("Voto secondo esame: " + esame2.getGrade());
-
-        WrittenAndOralExam esame3 = new WrittenAndOralExam("ciao3", 12);
-
-        esame3.setOralGrade(30);
-        esame3.setWrittenGrade(30);
-
-        System.out.println("Voto terzo esame: " + esame3.getGrade());
-
-        Student s = new Student("luca", 2);
-
-        s.addExam(esame);
-        s.addExam(esame2);
-        s.addExam(esame3);
-
-        System.out.println("Media pesata calcolata su esami precedenti: " + s.getECTSAverage());
-
+        ComputerScienceDegree cs = new ComputerScienceDegree();
+        try {
+            cs.load("./students.csv");
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        System.out.println(cs);
     }
 }
