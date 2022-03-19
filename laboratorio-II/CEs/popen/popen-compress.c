89f6c26aae6877f0dcd58c44e8e88877cb78d7b7
Mitra98t <personalmailfm98@gmail.com>
2022-03-19 10:30:23 +0100

    sync

--- laboratorio-II/CEs/popen/popen-compress.c
+++ laboratorio-II/CEs/popen/popen-compress.c
@@ -7,62 +7,69 @@
 #define MAX_FILENAME_LENGTH 255
 #endif
 
-#define CHUNKSIZE  16384
+#define CHUNKSIZE 16384
 
-int main(int argc, char *argv[]) {
-  if (argc == 1) {
+int main(int argc, char *argv[])
+{
+  if (argc == 1)
+  {
     fprintf(stderr, "use: %s file [file]\n", argv[0]);
     return EXIT_FAILURE;
   }
-  
+
   const int len = MAX_FILENAME_LENGTH + 64;
 
   char *buffer = malloc(CHUNKSIZE);
-  if (!buffer) {
+  if (!buffer)
+  {
     perror("malloc");
     return EXIT_FAILURE;
   }
-  
-  for(int i=1; i<argc; ++i) {  
-    const char *filename = argv[i];    
+
+  for (int i = 1; i < argc; ++i)
+  {
+    const char *filename = argv[i];
     int r;
-    if ((r=isRegular(filename, NULL)) == 1) {
+    if ((r = isRegular(filename, NULL)) == 1)
+    {
       FILE *fin;
-      if ((fin = fopen(filename, "r")) == NULL) {
-	perror("fopen");
-	continue;
+      if ((fin = fopen(filename, "r")) == NULL)
+      {
+        perror("fopen");
+        continue;
       }
-      
+
       char str[len];
       snprintf(str, len, "gzip - -c > %s.gz", filename);
       FILE *fpipe;
-      if ((fpipe = popen(str, "w")) == NULL) {
-	perror("popen");
-	continue;
+      if ((fpipe = popen(str, "w")) == NULL)
+      {
+        perror("popen");
+        continue;
       }
 
-      while(!feof(fin)) {
-	size_t sz = fread(buffer, 1, CHUNKSIZE, fin);
-	if (sz>0)
-	  if (fwrite(buffer, 1, sz, fpipe) != sz) {
-	    perror("fwrite");
-	    break;
-	  }
+      while (!feof(fin))
+      {
+        size_t sz = fread(buffer, 1, CHUNKSIZE, fin);
+        if (sz > 0)
+          if (fwrite(buffer, 1, sz, fpipe) != sz)
+          {
+            perror("fwrite");
+            break;
+          }
       }
-      if (pclose(fpipe) == -1) {
-	perror("pclose");
+      if (pclose(fpipe) == -1)
+      {
+        perror("pclose");
       }
       continue;
     }
-    if (r==0) 
+    if (r == 0)
       fprintf(stderr, "%s is not a regular file\n", filename);
-    else 
-      perror("isRegular");    
+    else
+      perror("isRegular");
   }
-  
+
   free(buffer);
   return 0;
 }
-
-
-  
