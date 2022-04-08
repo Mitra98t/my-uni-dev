70932fa6aff52bd810ecc81a6d130440d2a1ec35
Mitra98t <personalmailfm98@gmail.com>
2022-04-08 11:51:53 +0200

    thread

--- laboratorio-II/CEs/threadPipes/main.c
+++ laboratorio-II/CEs/threadPipes/main.c
@@ -1,6 +1,8 @@
 #define _GNU_SOURCE
 #include <pthread.h>
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 #include "unboundedqueue.h"
 
 void *TH1(void *);
@@ -23,23 +25,30 @@ int main(int argc, char const *argv[])
 {
     if (argc != 2)
         return 1;
-    char *file = argv[1];
+    char *file = strdup(argv[1]);
     pthread_t threads[3];
     Queue_t *q1 = initQueue();
     Queue_t *q2 = initQueue();
 
     argTh1 *args1 = (argTh1 *)malloc(sizeof(argTh1));
-    args1->file = file;
+    args1->file = strdup(file);
     args1->q1 = q1;
-    pthread_create(threads[0], NULL, TH1, args1);
+    pthread_create(&threads[0], NULL, TH1, args1);
 
     argTh2 *args2 = (argTh2 *)malloc(sizeof(argTh2));
     args2->q1 = q1;
     args2->q2 = q2;
-    pthread_create(threads[0], NULL, TH2, args2);
+    pthread_create(&threads[1], NULL, TH2, args2);
 
-    pthread_create(threads[0], NULL, TH3, q2);
+    pthread_create(&threads[2], NULL, TH3, q2);
 
+    for (int i = 0; i < 3; i++)
+    {
+        pthread_join(threads[i], NULL);
+    }
+
+    deleteQueue(q1);
+    deleteQueue(q2);
     return 0;
 }
 
@@ -50,16 +59,22 @@ void *TH1(void *args)
     size_t len = 0;
     ssize_t read;
     argTh1 *stru = (argTh1 *)args;
-    ptr = fopen(stru->file, 'r');
+
+    printf("In THREAD 1: %s\n", stru->file);
+
+    ptr = fopen(stru->file, "r");
 
     if (ptr == NULL)
     {
+        printf("errore?\n");
         push(stru->q1, "<<ERROR");
-        return 1;
+        return NULL;
     }
 
     while ((read = getline(&line, &len, ptr)) != -1)
     {
+        printf("Line: %s\n", line);
+        fflush(stdout);
         push(stru->q1, line);
     }
 
@@ -68,7 +83,7 @@ void *TH1(void *args)
     if (line)
         free(line);
 
-    return 0;
+    return NULL;
 }
 void *TH2(void *args)
 {
@@ -82,7 +97,7 @@ void *TH2(void *args)
         if (strcmp(line, "<<ERROR") == 0)
         {
             push(stru->q2, "<<ERROR");
-            return 1;
+            return NULL;
         }
         if (strcmp(line, "<<EOF") == 0)
         {
@@ -102,7 +117,7 @@ void *TH2(void *args)
             token = strtok(NULL, s);
         }
     }
-    return 0;
+    return NULL;
 }
 void *TH3(void *args)
 {
@@ -112,12 +127,12 @@ void *TH3(void *args)
     {
         if (strcmp(line, "<<ERROR") == 0)
         {
-            return 1;
+            return NULL;
         }
         if (strcmp(line, "<<EOF") == 0)
             break;
 
         printf("%s\n", line);
     }
-    return 0;
+    return NULL;
 }
