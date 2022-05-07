26d8b3c34f2bd8631c0ddb662988ec7e2e96c875
Mitra98t <personalmailfm98@gmail.com>
2022-05-07 12:24:19 +0200

    Organizzato Lab II

--- laboratorio-II/C/avanzato/homeworks/dec.c
+++ laboratorio-II/C/avanzato/homeworks/dec.c
@@ -1,6 +1,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <util.h>
+#include <unistd.h>
+#include <errno.h>
 
 #define SYSCALL(name, r, sc, str, ...)	SYSCALL_EXIT(name,r,sc,str,__VA_ARGS__)
 
@@ -11,11 +13,11 @@ int main(int argc, char *argv[]) {
     if (argc>1) {
 	x = atoi(argv[1]);
 	if (x<0) goto fine;
-	SYSCALL(write, r, write(1, &x,sizeof(x)), "Processo %d scrivendo: errno=%d\n", getpid(), errno);
+	SYSCALL_EXIT(write, r, write(1, &x,sizeof(x)), "Processo %d scrivendo: errno=%d\n", getpid(), errno);
     }
 
     do {
-	SYSCALL(read,r,read(0, &x,sizeof(x)),"Processo %d leggendo: errno=%d\n", getpid(), errno);
+	SYSCALL_EXIT(read,r,read(0, &x,sizeof(x)),"Processo %d leggendo: errno=%d\n", getpid(), errno);
 	if (r==0) { 
 	    fprintf(stderr, "Processo %d, esco perche' lo standard input e' chiuso!\n", getpid());
 	    return 0;
@@ -24,7 +26,7 @@ int main(int argc, char *argv[]) {
 	--x;
 	if (x<0)  break;
 
-	SYSCALL(write, r, write(1, &x,sizeof(x)), "Processo %d scrivendo: errno=%d\n", getpid(), errno);
+	SYSCALL_EXIT(write, r, write(1, &x,sizeof(x)), "Processo %d scrivendo: errno=%d\n", getpid(), errno);
     } while(1);
     
  fine:
